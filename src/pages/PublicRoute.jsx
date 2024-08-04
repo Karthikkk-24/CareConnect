20b6ff1f278f6981dc7fe2bccd611a963427fa88
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-04 21:10:45 +0530

    Added Public Route

--- src/pages/PublicRoute.jsx
+++ src/pages/PublicRoute.jsx
@@ -1,9 +1,16 @@
 
-
+import { Outlet } from 'react-router-dom';
 export default function PublicRoute() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-center">
+            <div className="w-full h-full flex items-start justify-start">
+                <div className="w-96 h-full flex flex-col items-start justify-start">
+                </div>
+                <div className="w-full h-full flex flex-col items-start justify-start">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    );
 }
